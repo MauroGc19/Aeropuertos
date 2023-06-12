cfee55f4bc698d148891f3a9ef95e6feebdc46b2
mauricio <tu@email.com>
2023-06-12 15:52:57 -0500

    tercera version

--- Prueba.py
+++ Prueba.py
@@ -1,15 +1,18 @@
 from tkinter import *
 
 
-def seleccionar_opcion(opcion):
-    GUI.guardar_opcion(GUI,opcion)
+def seleccionar_origen(opcion):
+    GUI.guardar_origen(GUI,opcion)#guarda la opcion seleccionada y la manda a la clase gui para que se pueda usar en esta
+def seleccionar_destino(opcion):
+    GUI.guardar_destino(GUI,opcion)#guarda la opcion seleccionada y la manda a la clase gui para que se pueda usar en esta
 def seleccionar_color(color):
-    GUI.guardar_color(GUI, color)
+    GUI.guardar_color(GUI, color)#guarda la opcion seleccionada y la manda a la clase gui para que se pueda usar en esta
     
 
 class GUI:
     def __init__(self):
-        self.lista=[]
+        self.lista_trayectos=[] #lista donde se guardan los trayectos
+        self.lista=[] # lista donde se guardan los aero puertos
         self.ventana = Tk()
         self.ventana.title("Círculo")
         self.ventana.geometry("1000x500")
@@ -28,10 +31,59 @@ class GUI:
 
         self.button = Button(self.ventana, text="Agregar círculo", command=self.open_form)
         self.button.pack()
+        self.button = Button(self.ventana, text="Crear Trayecto", command=self.open_trayecto)
+        self.button.pack()
+
 
         self.ventana.mainloop()
+    
+    def open_trayecto(self):
+        self.formulario = Toplevel(self.ventana)
+        self.formulario.title("Crear trayecto")
+        lista=self.lista_Nombres()
         
-
+        letrero = Label(self.formulario, text="origen")
+        letrero.pack()
+        
+        opciones = StringVar(self.formulario)
+        opciones.set("ninguno")
+        
+        opcion_menu = OptionMenu(self.formulario, opciones, *lista, command=seleccionar_origen)
+        opcion_menu.pack()
+        
+        letrero2 = Label(self.formulario,text="Destino")
+        letrero2.pack()
+        
+        opciones = StringVar(self.formulario)
+        opciones.set("ninguno")
+        
+        opcion_menu = OptionMenu(self.formulario, opciones, *lista, command=seleccionar_destino)
+        opcion_menu.pack()
+        
+        letrero3= Label(self.formulario, text="Duracion")
+        letrero3.pack()
+        self.duracion = Entry(self.formulario)
+        self.duracion.pack()
+        
+        letrero4= Label(self.formulario, text="Distancia")
+        letrero4.pack()
+        self.distancia = Entry(self.formulario)
+        self.distancia.pack()
+        
+        submit_button = Button(self.formulario, text="crear", command=self.add_trayecto)
+        submit_button.pack()
+        
+    def add_trayecto(self):
+        origen=self.nombre_origen
+        destino=self.nombre_destino
+        duracion = self.duracion.get()
+        distacia = self.duracion.get()
+        if(origen==destino):
+            self.show_error_window("el origen ye destino deben ser diferentes")
+        else:
+            self.crear_linea(origen, destino, duracion, distacia)
+            self.formulario.destroy()
+    
     def open_form(self):
         self.form_window = Toplevel(self.ventana)
         self.form_window.title("Agregar círculo")
@@ -58,28 +110,24 @@ class GUI:
         color.set("ninguno")
         opcion_color = OptionMenu(self.form_window,color, *Colores, command=seleccionar_color )
         opcion_color.pack()
-        
-        letrero = Label(self.form_window, text="Trayecto")
-        letrero.pack()
-        lista=self.lista_Nombres()
-        opciones = StringVar(self.form_window)
-        opciones.set("ninguno")
-        opcion_menu = OptionMenu(self.form_window, opciones, *lista, command=seleccionar_opcion)
-        opcion_menu.pack()
 
         submit_button = Button(self.form_window, text="Guardar", command=self.add_circle)
         submit_button.pack()
-    def guardar_opcion(self,name):
-        self.nombre_opcion=name
+        
+    def guardar_origen(self,name):
+        self.nombre_origen=name
+    def guardar_destino(self,name):
+        self.nombre_destino=name
+        
     def guardar_color(self,color):
         self.color=color
+        
     def add_circle(self):
         name = self.name_entry.get()
         color = self.color
         pos_y = self.pos_y_entry.get()
         pos_x = self.pos_x_entry.get()
-        name_opcion=self.nombre_opcion
-        if name == '' or color == '' or pos_x == '' or pos_y == '' or name_opcion=='':
+        if name == '' or color == '' or pos_x == '' or pos_y == '':
             self.show_error_window("llena los datos faltantes")
         else:
             x=int(self.pos_x_entry.get())
@@ -87,7 +135,6 @@ class GUI:
             existe= self.comprobar(x,y, name)
             if(existe):
                 self.crear_circulo(x, y, color, name)
-                self.crear_linea(x,y,name_opcion)
                 self.form_window.destroy()
 
     def show_error_window(self, text):
@@ -101,23 +148,32 @@ class GUI:
         ok_button.pack()
 
     def crear_circulo(self, x, y, color, name ):
-        self.contenido=[]
+        contenido=[]
         radius = 30
         self.canvas.create_oval(x-radius, y-radius, x+radius, y+radius,fill=color)
         self.canvas.create_text(x, y, text=name, fill="white")
-        self.contenido.append(name)
-        self.contenido.append(color)
-        self.contenido.append(x)
-        self.contenido.append(y)
-        self.lista.append(self.contenido)
-    def crear_linea(self,x,y,name_opcion):
-        x2=None
-        y2=None
+        contenido.append(name)
+        contenido.append(color)
+        contenido.append(x)
+        contenido.append(y)
+        self.lista.append(contenido)
+    def crear_linea(self,origen, destino, duracion, distancia):
+        trayecto=[]
         for lista in self.lista:
-            if(lista[0]==name_opcion):
+            if(lista[0]==origen):
+                x=lista[2]
+                y=lista[3]
+        for lista in self.lista:
+            if(lista[0]==destino):
                 x2=lista[2]
                 y2=lista[3]
         self.canvas.create_line(x,y,x2,y2)
+        trayecto.append(origen)
+        trayecto.append(destino)
+        trayecto.append(duracion)
+        trayecto.append(distancia)
+        self.lista_trayectos.append(trayecto)
+        
     def comprobar(self,x,y,name):
         existe= True
         for lista in self.lista:
